843f0b6d72f7e48485bd06344137ee3a77c0f67f
lordinit <l1zzard2444@gmail.com>
2023-07-25 18:08:58 +0300

    upm2

--- m2/src/app.ts
+++ m2/src/app.ts
@@ -1,12 +1,13 @@
 import { M2Service } from './taskProcessor';
+import logger from './utils/logger';
 
 async function runM2Service() {
   try {
-    console.log('Сервис M2 запущен');
+    logger.info('Сервис M2 запущен');
     const m2Service = new M2Service();
     await m2Service.start();
   } catch (error) {
-    console.error('Ошибка при запуске сервиса M2:', (error as Error).message);
+    logger.error('Ошибка при запуске сервиса M2:', (error as Error).message);
   }
 }
 
